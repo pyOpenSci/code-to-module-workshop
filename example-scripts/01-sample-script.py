22d835321632faf7b443749b3c366fe11392f79f
Leah Wasser <leah@pyopensci.org>
2024-07-09 07:51:52 -0700

    Update 01-sample-script.py

--- example-scripts/01-sample-script.py
+++ example-scripts/01-sample-script.py
@@ -13,7 +13,7 @@ import pandas as pd
 data_path = "https://raw.githubusercontent.com/pyOpenSci/code-to-module-workshop/main/example-scripts/data.csv"
 data = pd.read_csv(data_path)
 data.columns
-# Print the first few rows of the data
+
 print(data.head())
 
 # Filter rows where the value in 'column1' is greater than 10
@@ -21,20 +21,13 @@ sites_more_snow = data[data["average_snowfall"] > 10]
 
 print(f"Sites with more than 15 inches of snow: \n{sites_more_snow}")
 
-
-# Calculate the mean of 'column2' for the filtered rows
 avg_temp_more_snow = sites_more_snow["average_temperature"].mean()
-
-# Print the result
 print(f"The mean temp for sites with more snow is {avg_temp_more_snow}")
 
 # Sites with less snow - calc average temp
 sites_less_snow = data[data["average_snowfall"] < 5]
-
-# Calculate the mean of 'column2' for the filtered rows
 avg_temp_less_snow = sites_less_snow["average_temperature"].mean()
 
-# Print the result
 print(f"The mean temp for sites with less snow is {avg_temp_less_snow}")
 
 
@@ -43,13 +36,10 @@ sites_medium_snow = data[
     (data["average_snowfall"] >= 5) & (data["average_snowfall"] <= 15)
 ]
 
-# Print sites with snowfall between 5 and 15 inches
 print(f"Sites with snowfall between 5 and 15 inches:\n{sites_medium_snow}")
 
 # Calculate the mean temperature for sites with medium snow
 avg_temp_medium_snow = sites_medium_snow["average_temperature"].mean()
-
-# Print the result
 print(
     f"The mean temperature for sites with medium snow is {avg_temp_medium_snow}"
 )
